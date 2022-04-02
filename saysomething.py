abf7c82bcf8cd9806e676e491f4db4cba38d1c35
TheLevic <levicrider08@gmail.com>
2022-04-02 00:07:09 -0500

    Small Update

--- saysomething.py
+++ saysomething.py
@@ -2,7 +2,9 @@ import gtts
 from playsound import playsound
 
 def sayThings():
-    while (input != 999):
+    val = "123"
+    gtts.lang.tts_langs() 
+    while (val != "999"):
         val = input("Enter what you want him to say: ")
         tts = gtts.gTTS(val)
         sound = "say.mp3"
